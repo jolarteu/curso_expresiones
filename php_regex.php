2f30fb02b79f85005d1375ea07cfd41a003f6308
juanse1432 <jolarteu@unbosque.edu.co>
2021-06-26 04:56:16 -0500

    resultados php

--- php_regex.php
+++ php_regex.php
@@ -4,14 +4,16 @@ $file = fopen("resultados.csv","r");
 $match   = 0;
 $nomatch = 0;
 
+$t = time();
 while(!feof($file)) {
 
     $line=fgets($file);
   #  echo $line ;
 
-    if(preg_match(
+#1957-09-25,Netherlands,Austria,1,1,FIFA World Cup qualification,Amsterdam,Netherlands,FALSE
 
-        '/^2018\-01\-(\d\d),.*$/',
+    if(preg_match(
+        '/^\d{4,4}.*,(.*?),(.*?),(\d+),(\d+),.*$/i',
         $line,
         $m
       )                           // if(preg_match(
@@ -22,13 +24,26 @@ while(!feof($file)) {
                                   //   )
       )
       {
-        print_r($m);
-        $match++;
+      if($m[3] == $m[4]){
+        echo "Empate   ";
+      }
+      elseif ($m[3] < $m[4]){
+        echo "Visitante";
+      }
+      else {
+        echo "Local     ";
+      }
+      print_r("\t$m[1], $m[2] ($m[3]-$m[4])\n");
+      $match++;
       }
       else {
         $nomatch++;
+        // echo $line;
+        // echo "holi\n";
+
       }
 }
 
 fclose($file);
 printf("\n %d match y %d no match\n", $match, $nomatch);
+printf("tiempo: %d\n", time() - $t);
