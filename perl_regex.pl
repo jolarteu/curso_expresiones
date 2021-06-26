03189e13d7213eebea27916f539f78c0fed8afcd
juanse1432 <jolarteu@unbosque.edu.co>
2021-06-26 03:09:51 -0500

    buscando por fecha

--- perl_regex.pl
+++ perl_regex.pl
@@ -8,10 +8,28 @@ my $t= time;
 
 open(my $f, "<resultados.csv") or die("no hay archivo");
 
+my $match =   0;
+my $nomatch=  0;
+
 while(<$f>) {
 
-  print $_;
+#1957-09-25,Netherlands,Austria,1,1,FIFA World Cup qualification,Amsterdam,Netherlands,FALSE
+  chomp;
+  #if(m/^\d{4,4}\-02.*$/){
 
+  if(m/^.*?,(*?),(*?),(*?),(*?),.*$/){
+
+
+
+    print $_."\n";
+    $match++;
+  }
+
+  else {
+    $nomatch ++;
+  }
 }
 
 close ($f);
+
+print("se encontraron $match matches y $nomatch no matches \n");
