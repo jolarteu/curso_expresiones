9d57abbcb32f7b94e70b8d3189cc1f291ec32b82
juanse1432 <jolarteu@unbosque.edu.co>
2021-06-27 06:26:42 -0500

    busqueda de cantidad de goles con python

--- python_regex.py
+++ python_regex.py
@@ -2,8 +2,10 @@
 
 import re
 #2018-06-04,Italy,Netherlands,1,1,Friendly,Turin,Italy,FALSE
-pattern= re.compile(r'^(\d{4,4})-(.*?),(.*),Friendly,.*$')
+pattern= re.compile(r'^(\d{4,4})-(.*?),(.+?)(\d+),(\d+).*$')
 
+i=0
+j=0
 
 f= open("resultados.csv", "r", encoding='utf-8')
 #print(type(f))
@@ -11,7 +13,15 @@ for line in f:
     res = re.match(pattern, line)
 
     if res:
-        pass
-        print (res.group(3))
+        t=int(res.group(4))+int(res.group(5))
+        if t>20:
+            j=1+j
+            #print(t)
+            print (line)
 
+        #print (res.group(4))
+    i=1+i
+
+print(i)
+print(j)
 f.close
